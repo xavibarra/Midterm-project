27de13f3e9008dcefc0864225d45fedaf26d17ba
Xavier Barrachina <xbarrachinafont@gmail.com>
2022-11-25 13:15:44 +0100

    projects-section

---
+++ script/index.js
@@ -0,0 +1,19 @@
+const getExternalAPiInfo = () => {
+    // Part 1 - Llamamos al recurso
+    fetch("https://jsonplaceholder.typicode.com/posts/")
+        
+        .then((response) => response.json())
+        .then((data) => {
+            document.querySelector("#simplify").innerHTML += data[0].title.slice(1,5);
+            document.querySelector("#simplifySub").innerHTML += data[0].body.slice(0,20);
+
+            document.querySelector("#dashcoin").innerHTML += data[1].title.slice(1,5);
+            document.querySelector("#dashcoinSub").innerHTML += data[1].body.slice(0,20);
+
+            document.querySelector("#vectorify").innerHTML += data[2].title.slice(1,5);
+            document.querySelector("#vectorifySub").innerHTML += data[2].body.slice(0,20);
+          })
+          .catch((error) => console.log(error));;
+}
+
+getExternalAPiInfo();
